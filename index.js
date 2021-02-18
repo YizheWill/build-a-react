a30852620795692ad690fde881252ab12692b1d9
Yizhe Wang <yizhe87@gmail.com>
2021-02-17 22:34:28 -0800

    start adding state

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
 import { createElement, Component, render } from './toy-react';
 
 class MyComponent extends Component {
+  constructor() {
+    super();
+    this.state = { a: 1, b: 2 };
+  }
   render() {
     return (
       <div>
         <h1>My component</h1>
+        <span>{this.state.a.toString()}</span>
         {this.children}
       </div>
     );
